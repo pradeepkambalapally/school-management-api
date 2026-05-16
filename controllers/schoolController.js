781f2417edd74021502938dfe7c0b26b5ec49205
pradeepkambalapally <pradeepkambalapally2004@gmail.com>
2026-05-16 12:36:06 +0530

    Implemented listSchools API with distance sorting

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -1,6 +1,20 @@
 const db = require('../db');
 
-exports.addSchool = (req, res) => {
+const calculateDistance = (lat1, lon1, lat2, lon2) => {
+    const R = 6371;
+    
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c;
+}
+
+const addSchool = (req, res) => {
     const {name, address, latitude, longitude} = req.body;
 
     if(!name || !address || latitude == null || longitude == null){
@@ -29,4 +43,41 @@ exports.addSchool = (req, res) => {
     )
 }
 
+const listSchools = (req, res) => {
+    const {latitude, longitude} = req.query;
 
+    if(!latitude || !longitude){
+        return res.status(400).json({
+            message : "Latitude and Longitude required",
+            success : false
+        })
+    }
+
+    db.query("SELECT * FROM schools", (err, results) => {
+        if(err){
+            return res.status(500).json({
+                message : "Database error",
+                success : false
+            })
+        }
+
+        const schoolsWithDistance = results.map((school) => {
+            const distance = calculateDistance(parseFloat(latitude), parseFloat(longitude), school.latitude, school.longitude);
+
+            return {
+                ...school,
+                distance,
+            }
+        });
+
+        schoolsWithDistance.sort((a, b) => 
+        a.distance - b.distance);
+
+        res.json(schoolsWithDistance);
+    })
+}
+
+module.exports = {
+    addSchool,
+    listSchools
+}
--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,8 +1,13 @@
 
 const express = require("express");
-const { addSchool } = require("../controllers/schoolController");
+const { addSchool, listSchools } = require("../controllers/schoolController");
 
 const router = express.Router();
 
 
 router.post("/addSchool", addSchool);
+
+router.get("/listSchools", listSchools);
+
+
+module.exports = router;
