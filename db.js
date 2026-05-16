f3c2f1dfe44b60dbee8a57e3544c06aecb8250f5
pradeepkambalapally <pradeepkambalapally2004@gmail.com>
2026-05-16 13:47:46 +0530

    Update db configuration for Render deployment

--- db.js
+++ db.js
@@ -2,19 +2,20 @@ require('dotenv').config();
 
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host : process.env.DB_HOST,
+const connection = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
 
-connection.connect((err) => {
-    if(err){
+connection.getConnection((err) => {
+    if (err) {
         console.log("Database connection failed");
         console.log(err);
-    }else{
-        console.log("MySQL Connection");
+    } else {
+        console.log("MySQL Connected");
     }
 });
 
